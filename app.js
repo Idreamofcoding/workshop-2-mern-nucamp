3c700208ee92f099f56f6ad5ce764299eb7d0d4a
albertrocha <rocharocharocha95@yahoo.com>
2022-10-22 15:47:07 -0400

    ended bootcamp but need to retake the course. next lessons is user authentication with passport and json web token

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,6 +45,7 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
 
+
 app.use(session({
   name: 'session-id',
   secret: '12345-67890-09876-54321',
@@ -50,26 +53,22 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.session);
 
-  if (!req.session.user) {
-      const err = new Error('You are not authenticated!');
+function auth(req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+      const err = new Error('You are not authenticated!');                    
       err.status = 401;
       return next(err);
-
   } else {
-      if (req.session.user === 'authenticated') {
-          return next();
-      } else {
-          const err = new Error('You are not authenticated!');
-          err.status = 401;
-          return next(err);
-      }
+      return next();
   }
 }
 
